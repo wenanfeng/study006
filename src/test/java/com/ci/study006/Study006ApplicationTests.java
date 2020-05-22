605895e88a84d59343d6e6367c095f4d69cb12bb
wenanfeng <wenanfeng@ebupt.com>
2020-05-22 16:22:17 +0800

    init

---
+++ src/test/java/com/ci/study006/Study006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci.study006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study006ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
